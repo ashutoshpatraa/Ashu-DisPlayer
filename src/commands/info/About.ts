3505147d319abe6021dc7d2d8c8a123204934ca2
Poler Flyer <ashutoshpatra616@gmail.com>
2026-01-27 21:46:45 +0530

    Update About.ts

--- src/commands/info/About.ts
+++ src/commands/info/About.ts
@@ -40,36 +40,27 @@ export default class About extends Command {
 			.setURL(
 				`https://discord.com/api/oauth2/authorize?client_id=${client.env.CLIENT_ID}&permissions=8&scope=bot%20applications.commands`,
 			);
-		const supportButton = new ButtonBuilder()
-			.setLabel(ctx.locale(I18N.buttons.support))
-			.setStyle(ButtonStyle.Link)
-			.setURL("https://discord.gg/YQsGbTwPBx");
-		const row = new ActionRowBuilder<ButtonBuilder>().addComponents(inviteButton, supportButton);
+		const row = new ActionRowBuilder<ButtonBuilder>().addComponents(inviteButton);
 		const embed = this.client
 			.embed()
 			.setAuthor({
-				name: "Lavamusic",
+				name: "Ashu's DisPlayer",
 				iconURL:
-					"https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png",
+					"https://i.imgur.com/ud3EWNh.jpg",
 			})
 			.setThumbnail(
-				"https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png",
+				"https://i.imgur.com/ud3EWNh.jpg",
 			)
 			.setColor(this.client.color.main)
 			.addFields(
 				{
 					name: ctx.locale(I18N.commands.about.fields.creator),
-					value: "[appujet](https://github.com/appujet)",
+					value: "[Ashu](https://github.com/ashutoshpatraa)",
 					inline: true,
 				},
 				{
 					name: ctx.locale(I18N.commands.about.fields.repository),
-					value: "[Here](https://github.com/appujet/lavamusic)",
-					inline: true,
-				},
-				{
-					name: ctx.locale(I18N.commands.about.fields.support),
-					value: "[Here](https://discord.gg/YQsGbTwPBx)",
+					value: "[Here](https://github.com/ashutoshpatraa/ashus-displayer)",
 					inline: true,
 				},
 				{
@@ -87,12 +78,8 @@ export default class About extends Command {
 }
 
 /**
- * Project: lavamusic
- * Author: Appu
- * Main Contributor: LucasB25
- * Company: Coders
+ * Project: Ashu's DisPlayer
+ * Author: Ashu
+ * GitHub: https://github.com/ashutoshpatraa
  * Copyright (c) 2024. All rights reserved.
- * This code is the property of Coder and may not be reproduced or
- * modified without permission. For more information, contact us at
- * https://discord.gg/YQsGbTwPBx
  */
